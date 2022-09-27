d1d3d6f93ffcca7c597f0dad055ab840876387ae
Nicolas Cely <nikocely24@gmail.com>
2022-09-26 23:42:10 -0500

    optimizando el proyecto final

--- index.js
+++ index.js
@@ -1,88 +1,75 @@
-const selectTag = document.getElementById('lista') 
+const productos = []
+const carrito = []
+
+class Producto{
+    constructor(id, nombre, precio){
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+    }
+    desplegarProductos(){
+        const tarjeta = 
+        `<div class="card" style="width: 18rem;>
+        <div class="card-body">
+          <h5>${this.nombre}</h5>
+          <p>${this.precio}</p>
+          <div class="btn-container">
+            <button id=${this.id} class="btnAgregar">Agregar al carrito</button>
+          </div>
+        </div>
+    </div>`
+        const container = document.getElementById('container')
+        container.innerHTML += tarjeta
+    }
+    agregarEvento(){
+        const btnAgregar = document.getElementById(this.id)
+        const productoEncontrado = productos.find(product => product.id == this.id)
+        btnAgregar.addEventListener('click', () => AgregarAlCarrito(productoEncontrado))
+    }
+    }
+
+
 
 //objetos que se guardan en el arreglo 'productos'
-let productos =[{
-    nombre: 'Libra de cáfe',
-    Precio: 25000},
-    {
-        nombre: '1/2 Libra de cáfe',
-        Precio: 15000
-    },
-    {
-        nombre: 'Prensa francesa de plástico',
-        Precio: 25000
-    },
-    {
-        nombre: 'Prensa francesa de madera',
-        Precio: 55000
-    },
-    {
-        nombre: 'Aeropress',
-        Precio: 110000
-    },
-    {
-        nombre: 'Syphon Japonesa',
-        Precio: 165000
-    },
-    {
-        nombre: 'V60 en acrilico',
-        Precio: 30000
-    },
-    {
-        nombre: 'V60 en cerámica',
-        Precio: 50000
-    },
-    {
-        nombre: 'Molino de acero',
-        Precio: 60000
-    },
-    {
-        nombre: 'Jarra Kettle',
-        Precio: 85000
-    }
-]
+let prod1 = new Producto('01', 'Libra de cáfe', 25000)
+let prod2 = new Producto('02','1/2 Libra de cáfe',15000)
+let prod3 = new Producto('03','Prensa francesa de plástico', 25000)
+let prod4 = new Producto('04','Prensa francesa de madera', 55000)
+let prod5 = new Producto('05','Aeropress', 110000)
+let prod6 = new Producto('06','Syphon Japonesa', 165000)
+let prod7 = new Producto('07','V60 en acrilico', 30000)
+let prod8 = new Producto('08','V60 en cerámica', 50000)
+let prod9 = new Producto('09','Molino de acero', 60000)
+let prod10 = new Producto('10','Jarra Kettle', 85000)
 
-//Se agregan los elementos del arreglo productos al select
-productos.forEach(producto=>{
-    const option = document.createElement('option')
-    option.innerText = `${producto.nombre}: $${producto.Precio} `
-    selectTag.append(option)
-})
+productos.push(prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9, prod10)
 
-//Eventos
-const boton = document.createElement('button')
-boton.innerText = 'COMPRAR'
-document.body.append(boton)
+console.log(productos)
 
-const boton2 = document.createElement('button')
-boton2.innerText = 'TERMINAR COMPRA'
-document.body.append(boton2)
-
-let carrito = []
-
-//Evento para guardar el producto comprado en el arreglo carrito, guardar los productos comprados en el 
-//storage y recuperarlos y mostrarlos en el HTML
-boton.onclick = ()=>{
-    const productoSeleccionado = productos[selectTag.selectedIndex]
-    carrito.push(productoSeleccionado)
-
-    localStorage.setItem("productosCarrito",JSON.stringify(carrito))
-    const productosComprados = JSON.parse(localStorage.getItem("productosCarrito"))
-
-    productosComprados.forEach(dato=>{
-    const p = document.createElement('p')
-    p.innerText = `${dato.nombre}: $${dato.Precio}`
-    document.body.append(p)})
+productos.forEach(e => {
+   e.desplegarProductos()
 }
+)
+productos.forEach(e => {
+    e.agregarEvento()
+ }
+ )
+
+ function AgregarAlCarrito(producto){
+    console.log(producto)
+ }
 
 
 
-//Evento para calcular el precio total de la compra
+
+
+
+/*Evento para calcular el precio total de la compra
     boton2.onclick = ()=>{
     let totalCompra = 0
     carrito.forEach((prod)=>{
         totalCompra = totalCompra + prod.Precio
     }
     )
-    alert (`El precio total es ${totalCompra}`)}
+    alert (`El precio total es ${totalCompra}`)} */
 
